e62a6eeda411eb68812aba8220fdd6022d8c3b81
Valentin DEUSSEN <valentin.deussen@viacesi.fr>
2023-01-24 14:18:28 +0100

    Connexion to the database

--- FoodRat_back/build/index.js
+++ FoodRat_back/build/index.js
@@ -1,2 +1,23 @@
 "use strict";
-console.log('Hello world!');
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+// @ts-ignore
+const dotenv_1 = __importDefault(require("dotenv"));
+// @ts-ignore
+const express_1 = __importDefault(require("express"));
+const app = (0, express_1.default)();
+dotenv_1.default.config();
+const port = process.env.PORT || 6000;
+app.use(express_1.default.json());
+app.get('/', (req, res) => {
+    res.json({ message: "Welcome in FoodRat" });
+});
+// db connection
+const db = require("./models/dbconnexion");
+// const ApplicationRoute = require('./routes/application.route');
+// app.use("/application", ApplicationRoute);
+app.listen(port, () => {
+    return console.log(`Express is listening at http://localhost:${port}`);
+});
--- FoodRat_back/src/index.ts
+++ FoodRat_back/src/index.ts
@@ -1,3 +1,24 @@
+// @ts-ignore
+import dotenv from 'dotenv';
+// @ts-ignore
+import express from 'express';
+const app = express();
 
-console.log('Hello world!')
-console.log('Hello world!')
+dotenv.config();
+const port = process.env.PORT || 6000;
+
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.json({ message: "Welcome in FoodRat" });
+});
+
+// db connection
+const db = require("./models/dbconnexion");
+// const ApplicationRoute = require('./routes/application.route');
+// app.use("/application", ApplicationRoute);
+
+app.listen(port, () => {
+    return console.log(`Express is listening at http://localhost:${port}`);
+});
